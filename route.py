67dcf826c9d3512123f3803a6b984a82412f2cad
Gotzillaz Chinthanet <got.bodin@gmail.com>
2015-04-27 19:23:24 +0700

    try to get data from database

--- route.py
+++ route.py
@@ -4,12 +4,20 @@ import json, sqlite3
 
 conn = sqlite3.connect('db.sqlite3')
 conn.execute('PRAGMA foreign_keys = ON')
+c = conn.cursor()
+
 app = Flask(__name__)
 
 @app.route("/")
 @app.route("/<name>")
 def hello(name=None):
     return render_template('hello.html', name=name)
+
+@app.route("/sql")
+def sql():
+    c.execute("SELECT * FROM Film")
+    return str(c.fetchall())
+    #return str(len(st))
 
 def objToJSON(st) :
     return json.dumps(st)
