b1be534b4eb583c383be171d6b7882dc79a2b531
Gotzillaz Chinthanet <got.bodin@gmail.com>
2015-05-05 21:18:22 +0700

    update table name & column name

--- table.sql
+++ table.sql
@@ -1,63 +1,56 @@
-CREATE TABLE Film (
-  FilmID        INTEGER     NOT NULL,
+CREATE TABLE Video (
+  VideoID       INTEGER     NOT NULL,
   Type          TEXT        NOT NULL,
   Name          TEXT        NOT NULL,
   Year          INTEGER     NOT NULL,
   Plot          TEXT        NOT NULL,
   Rating        REAL        NOT NULL,
-  PRIMARY KEY (FilmID)
+  PRIMARY KEY (VideoID)
 );
-
 CREATE TABLE Detail (
   DetailID      INTEGER     NOT NULL,
-  FilmID        INTEGER     NOT NULL,
+  VideoID       INTEGER     NOT NULL,
   Language      TEXT        NOT NULL,
   ReleaseDate   TEXT        NOT NULL,
   Runtime       INTEGER     NOT NULL,
   AspectRatio   TEXT        NOT NULL,
   PRIMARY KEY (DetailID),
-  FOREIGN KEY (FilmID) REFERENCES Film(FilmID)
+  FOREIGN KEY (VideoID) REFERENCES Video(VideoID)
 );
-
 CREATE TABLE Episode (
   EpisodeID     INTEGER     NOT NULL,
-  FilmID        INTEGER     NOT NULL,
+  VideoID       INTEGER     NOT NULL,
   Name          TEXT        NOT NULL,
   Number        INTEGER     NOT NULL,
   Season        INTEGER     NOT NULL,
   PRIMARY KEY (EpisodeID),
-  FOREIGN KEY (FilmID) REFERENCES Film(FilmID)
+  FOREIGN KEY (VideoID) REFERENCES Video(VideoID)
 );
-
 CREATE TABLE Company (
   CompanyID     INTEGER     NOT NULL,
   Name          TEXT        NOT NULL,
   Country       TEXT        NOT NULL,
   PRIMARY KEY (CompanyID)
 );
-
-CREATE TABLE FilmCompany (
-  FilmID        INTEGER     NOT NULL,
+CREATE TABLE VideoCompany (
+  VideoID       INTEGER     NOT NULL,
   CompanyID     INTEGER     NOT NULL,
-  PRIMARY KEY (FilmID, CompanyID)
-  FOREIGN KEY (FilmID) REFERENCES Film(FilmID)
+  PRIMARY KEY (VideoID, CompanyID)
+  FOREIGN KEY (VideoID) REFERENCES Video(VideoID)
   FOREIGN KEY (CompanyID) REFERENCES Company(CompanyID)
 );
-
 CREATE TABLE Genre (
   GenreID       INTEGER     NOT NULL,
   Name          TEXT        NOT NULL,
   PRIMARY KEY (GenreID)
 );
-
-CREATE TABLE FilmGenre (
-  FilmID        INTEGER     NOT NULL,
+CREATE TABLE VideoGenre (
+  VideoID       INTEGER     NOT NULL,
   GenreID       INTEGER     NOT NULL,
-  PRIMARY KEY (FilmID, GenreID)
-  FOREIGN KEY (FilmID) REFERENCES Film(FilmID)
+  PRIMARY KEY (VideoID, GenreID)
+  FOREIGN KEY (VideoID) REFERENCES Video(VideoID)
   FOREIGN KEY (GenreID) REFERENCES Genre(GenreID)
 );
-
 CREATE TABLE Person (
   PersonID      INTEGER     NOT NULL,
   Name          TEXT        NOT NULL,
@@ -67,7 +60,6 @@ CREATE TABLE Person (
   Birth         TEXT        NOT NULL,
   PRIMARY KEY (PersonID)
 );
-
 CREATE TABLE Character (
   CharacterID   INTEGER     NOT NULL,
   PersonID      INTEGER     NOT NULL,
@@ -76,38 +68,33 @@ CREATE TABLE Character (
   PRIMARY KEY (CharacterID),
   FOREIGN KEY (PersonID) REFERENCES Person(PersonID)
 );
-
-CREATE TABLE FilmCharacter (
-  FilmID        INTEGER     NOT NULL,
+CREATE TABLE VideoCharacter (
+  VideoID       INTEGER     NOT NULL,
   CharacterID   INTEGER     NOT NULL,
-  PRIMARY KEY (FilmID, CharacterID)
-  FOREIGN KEY (FilmID) REFERENCES Film(FilmID)
+  PRIMARY KEY (VideoID, CharacterID)
+  FOREIGN KEY (VideoID) REFERENCES Video(VideoID)
   FOREIGN KEY (CharacterID) REFERENCES Character(CharacterID)
 );
-
-CREATE TABLE Direction (
-  FilmID        INTEGER     NOT NULL,
+CREATE TABLE Director (
+  VideoID       INTEGER     NOT NULL,
   PersonID      INTEGER     NOT NULL,
-  PRIMARY KEY (FilmID, PersonID)
-  FOREIGN KEY (FilmID) REFERENCES Film(FilmID)
+  PRIMARY KEY (VideoID, PersonID)
+  FOREIGN KEY (VideoID) REFERENCES Video(VideoID)
   FOREIGN KEY (PersonID) REFERENCES Person(PersonID)
 );
-
 CREATE TABLE Poster (
   PosterID      INTEGER     NOT NULL,
   Name          TEXT        NOT NULL,
   Image         BLOB        NOT NULL,
   PRIMARY KEY (PosterID)
 );
-
-CREATE TABLE FilmPoster (
-  FilmID        INTEGER     NOT NULL,
+CREATE TABLE VideoPoster (
+  VideoID       INTEGER     NOT NULL,
   PosterID      INTEGER     NOT NULL,
-  PRIMARY KEY (FilmID, PosterID)
-  FOREIGN KEY (FilmID) REFERENCES Film(FilmID)
+  PRIMARY KEY (VideoID, PosterID)
+  FOREIGN KEY (VideoID) REFERENCES Video(VideoID)
   FOREIGN KEY (PosterID) REFERENCES Poster(PosterID)
 );
-
 CREATE TABLE PersonPoster (
   PersonID      INTEGER     NOT NULL,
   PosterID      INTEGER     NOT NULL,
@@ -115,3 +102,4 @@ CREATE TABLE PersonPoster (
   FOREIGN KEY (PersonID) REFERENCES Person(PersonID)
   FOREIGN KEY (PosterID) REFERENCES Poster(PosterID)
 );
+
